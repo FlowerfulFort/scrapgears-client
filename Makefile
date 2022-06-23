4be3f5d914b7ff8cd048ac7f745bbd9dbf596768
FlowerfulFort <skagyghk@naver.com>
2022-06-23 15:19:44 +0900

    thread test

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CXX=g++
-FLAGS=-std=c++17 -Wall
+FLAGS=-std=c++17 -Wall -lpthread
 
 BIN=main
 SRC=$(wildcard src/*.cpp)
@@ -8,11 +8,9 @@ DIREC_OBJ=obj
 DIREC_SRC=src
 
 all: $(OBJ)
-	echo $(SRC)
 	$(CXX) $(FLAGS) -o $(BIN) $^
 
 $(DIREC_OBJ)/%.o: $(DIREC_SRC)/%.cpp
-	echo $(SRC)
 	$(CXX) -c $< -o $@
 
 clean:
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <pthread.h>
 
+pthread_t thread;
+
+void* run(void* params) {
+    std::cout << "thread created" << std::endl;
+}
 int main(void) {
     std::cout << "hello" << std::endl;
+    
+    pthread_create(&thread, nullptr, run, nullptr);
+    pthread_join(thread, nullptr);
     return 0;
 }
