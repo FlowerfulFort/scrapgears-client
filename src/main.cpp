4e8a4f53f722599a925eba906fc9c107f530aa92
FlowerfulFort <skagyghk@naver.com>
2022-06-24 12:01:28 +0900

    HTTP req example code added.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
+#include <cstring>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <pthread.h>
+#include <unistd.h>
 #include "network.h"
 
 #define ERR(fmt, ...) fprintf(stderr, fmt, ##__VA_ARGS__)
 pthread_t thread;
+
+constexpr int BUF_SIZE = 2048;
 
 void* run(void* params) {
     std::cout << "thread created" << std::endl;
 }
 int main(void) {
     struct sockaddr_in serv, from;
+    socklen_t size;
     int sock, ac;
+    char buf[BUF_SIZE];
+
     std::cout << "hello" << std::endl;
     
     if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
@@ -32,15 +39,31 @@ int main(void) {
         ERR("bind failed\n");
         return -1;
     }
-    /*
     std::cout << "Listening.." << std::endl;
     listen(sock, 1);
-    ac = accept(sock, reinterpret_cast<struct sockaddr*>(&from), static_cast<socklen_t*>(sizeof(from)));
-    if (ac < 0) {
-        ERR("accept failed..\n");
-        return -1;
+    while(1) {
+        ac = accept(sock, reinterpret_cast<struct sockaddr*>(&from), &size);
+        if (ac < 0) {
+            ERR("accept failed..\n");
+            continue;
+        }
+        std::cout << "Connection Accepted at" << std::endl;
+        std::cout << from << std::endl;
+
+        int recv_len = recv(ac, buf, BUF_SIZE, 0);
+        std::cout << "Received Data Length: " << recv_len << std::endl;
+        std::cout << "From apponent: \n" << buf << std::endl;
+
+        const char* message = 
+            "HTTP/1.1 200 OK\n"
+            "Date: Fri, 24 Jun 2022 11:44:00 KST\n"
+            "Server: Apache/2.2.3\n"
+            "Content-Type: text/html\n"
+            "Content-Length: 40\n"
+            "Connection: close\n\n"
+            "<html><body>hello http req</body></html>";
+        send(ac, message, strlen(message), 0);
     }
-    */
     pthread_create(&thread, nullptr, run, nullptr);
     pthread_join(thread, nullptr);
     return 0;
